892056f8210c711d44905dd908d1346013e8c67f
wof526 <wof526@sookmyung.ac.kr>
2024-03-04 18:39:47 +0900

    3/4yura
    튜토리얼 만듬

--- LastArk_2024/Assets/YR/tutorialManage.cs
+++ LastArk_2024/Assets/YR/tutorialManage.cs
@@ -49,7 +49,7 @@ public class tutorial : MonoBehaviour
                 text1.SetMsg("평화로웠던 세계.");
                 break;
             case 2:
-                text1.SetMsg("수많은 과거의 역사가 말했듯이, 평화는 잠시였습니다.");
+                text1.SetMsg("수많은 과거의 역사가 말했듯이,"+"\n평화는 잠시였습니다.");
                 break;
             case 3:
                 image1.SetActive(false);
@@ -67,15 +67,15 @@ public class tutorial : MonoBehaviour
             case 6:
                 image3.SetActive(false);
                 image4.SetActive(true);
-                text1.SetMsg("반복되는 비극 속, 인간은 결국 금기를 범하고 말았습니다.");
+                text1.SetMsg("반복되는 비극 속,"+"\n인간은 결국 금기를 범하고 말았습니다.");
                 break;
             case 7:
-                text1.SetMsg("세상의 빛을 모두 지워버리는 [암흑] 이라는 무기를 사용하고 만 것입니다.");
+                text1.SetMsg("세상의 빛을 모두 지워버리는"+"\n [암흑] 이라는 무기를 사용하고 만 것입니다.");
                 break;
             case 8:
                 image4.SetActive(false);
                 image5.SetActive(true);
-                text1.SetMsg("세계는 어둠으로 가득 차고");
+                text1.SetMsg("세계는 어둠으로 가득 차고...");
                 break;
             case 9:
                 text1.SetMsg("풀 한 포기, 벌레 하나 남지 않았습니다.");
@@ -83,7 +83,7 @@ public class tutorial : MonoBehaviour
             case 10:
                 image5.SetActive(false);
                 image6.SetActive(true);
-                text1.SetMsg("그 수라장 속에서, 끝까지 마음의 빛을 잃지 않았던 아이가 있었습니다.");
+                text1.SetMsg("그 수라장 속에서,"+"\n끝까지 마음의 빛을 잃지 않았던 아이가 있었습니다.");
                 break;
             case 11:
                 image6.SetActive(false);
@@ -92,7 +92,7 @@ public class tutorial : MonoBehaviour
                 text1.SetMsg("어느날 그 아이의 앞에, 3명의 이방인이 갑자기 나타났습니다.");
                 break;
             case 12:
-                text1.SetMsg("그들은 각각 [암흑]에 굴하지 않는 한 줄기 빛과 물, 씨앗을 한 아이에게 남기고, 사라졌습니다.");
+                text1.SetMsg("그들은 각각 [암흑]에 굴하지 않는 한 줄기 빛과"+"\n 물, 씨앗을 한 아이에게 남기고, 사라졌습니다.");
                 break;
             case 13:
                 image7.SetActive(false);
