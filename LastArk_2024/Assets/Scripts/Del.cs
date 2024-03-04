596b5995e705d5e5a935963648ab98a1d48321c8
ally432 <ally432@naver.com>
2024-03-04 15:11:53 +0900

    news

--- LastArk_2024/Assets/Scripts/Del.cs
+++ LastArk_2024/Assets/Scripts/Del.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class Del : MonoBehaviour
 {
     public GameObject ne;
+    public static bool isNews = false;  // 뉴스가 생성되었나?
+
+    void Start()
+    {
+        ne.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(isNews){
+            ne.SetActive(true);
+            isNews = false;
+        }   
+    }
 
     public void Seen(){
         ne.SetActive(false);
